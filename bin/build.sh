46ce1a9c8a03fa5b5da24bece60d3af7235d0bf3
aaaaa <aaaa@gmail.com>
2020-03-05 15:05:45 +0800

    add echo log

--- bin/build.sh
+++ bin/build.sh
@@ -6,24 +6,30 @@ repo_url="github.com/yuanmomo/domain-list-community"
 speedtest_xml=${current_dir}/servers-source.xml
 
 if [[ ! $(command -v ${current_dir}/domain-list-community) ]] ; then
-  export GO111MODULE=on
-  cd ${root_dir}
-  go build -o ${current_dir}/domain-list-community main.go
+    echo "go build domain-list-community command ....."
+
+    export GO111MODULE=on
+    cd ${root_dir}
+    go build -o ${current_dir}/domain-list-community main.go
 fi
 
 # link file to $GOPATH
 if [[ ! -e "${GOPATH}/src/${repo_url}" ]] ; then
-  ln -s -f "${root_dir}" "${GOPATH}"/src/${repo_url}
+    ln -s -f "${root_dir}" "${GOPATH}"/src/${repo_url}
 fi
 
+echo "downloading speedtest file ....."
 curl --socks5 127.0.0.1:1087 -L -o "${speedtest_xml}" "https://c.speedtest.net/speedtest-servers-static.php" -H 'Accept-Encoding: gzip' --compressed
 perl -ne '/host="(.+):[0-9]+"/ && print "full:$1\n"' "${speedtest_xml}" | perl -ne 'print if not /^(full:([0-9]{1,3}\.){3}[0-9]{1,3})$/' | perl -ne 'print lc' | sort --ignore-case -u >> "${GOPATH}"/src/${repo_url}/data/ookla-speedtest
 
 # exec
+echo "exec  domain-list-community command ....."
 "${current_dir}"/domain-list-community
 
+echo "move dlc.dat to v2ray bin dir....."
 chmod -x "${root_dir}"/dlc.dat
 mv "${root_dir}"/dlc.dat $(/usr/local/bin/greadlink -f /usr/local/bin/v2ray | xargs dirname)/geosite.dat
 
+echo "reset files....."
 git checkout HEAD -- ${root_dir}/data/ookla-speedtest
 rm -f "${speedtest_xml}"
